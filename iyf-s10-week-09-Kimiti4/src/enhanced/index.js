14ae0bc4f290b55c846c2d410745f179e493b2e1
Kimiti4 <81751331+Kimiti4@users.noreply.github.com>
2026-09-23 01:21:27 +0300

    chore(j021): remove deleted feed page export

--- iyf-s10-week-09-Kimiti4/src/enhanced/index.js
+++ iyf-s10-week-09-Kimiti4/src/enhanced/index.js
@@ -5,7 +5,6 @@
 // Pages
 export { default as EnhancedLoginPage } from './pages/EnhancedLoginPage';
 export { default as EnhancedRegisterPage } from './pages/EnhancedRegisterPage';
-export { default as EnhancedFeedPage } from './pages/EnhancedFeedPage';
 
 // Components
 export { default as ConstellationBackground } from './components/ConstellationBackground';
